4de9716f09428f3aea3ac1f2e30b8fd90efe3760
SuperChed <springac85@gmail.com>
2022-01-27 09:41:31 -0500

    Create circ_calc.py
    test file for git

---
+++ circ_calc.py
@@ -0,0 +1,13 @@
+# # this is a comment
+# 
+# name = input("What is your name?")
+# quest = input("What is your quest?")
+# color = ['red', 'blue', 'green']
+# 
+# print(name, quest, color[2])
+
+
+# calculate area of a circle
+radius = input('enter radius in meters:')
+area = 3.142 * int(radius)**2
+print(area)
